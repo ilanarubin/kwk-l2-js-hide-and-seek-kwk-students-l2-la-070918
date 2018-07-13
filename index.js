2fd8ec08a3ff3f64e1114e1092062e38701b880b
ilanarubin <ilanarubin007@gmail.com>
2018-07-13 17:37:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  return document.getElementById('grand-node').querySelector('div div div div')
+  return document.getElementById('grand-node').querySelector('div div div')
 }
